bb0676ea2df83c734767192903ead3a88cff9ac4
Priyank Agrawal <agrawal.pr@husky.neu.edu>
2017-04-02 16:48:57 -0400

    complexity correction

--- src/LongestCommonSubstring.java
+++ src/LongestCommonSubstring.java
@@ -6,8 +6,8 @@
  * To find length of the longest common substring from two given strings
  * using dynamic programming
  *
- * Time complexity is O(n^2)
- * Space complexity is O(n^2)
+ * Time complexity is O(nm)
+ * Space complexity is O(nm)
  */
 public class LongestCommonSubstring {
 
@@ -22,9 +22,9 @@ public class LongestCommonSubstring {
     int calculateLength(char[] str1, char[] str2) {
         int[][] lengths = new int[str1.length + 1][str2.length + 1];
         int maxLength = Integer.MIN_VALUE;
-        for (int i = 1; i < str1.length; i++) {
-            for (int j = 1; j < str2.length; j++) {
-                if (str1[i] == str2[j]) {               //current characters of both strings are same
+        for (int i = 1; i <= str1.length; i++) {
+            for (int j = 1; j <= str2.length; j++) {
+                if (str1[i-1] == str2[j-1]) {               //current characters of both strings are same
                     lengths[i][j] = lengths[i - 1][j - 1] + 1;      //increase the previous common substring count by 1
                     if (lengths[i][j] > maxLength)      //current common substring count is greater than previous one
                         maxLength = lengths[i][j];      //update as longest common substring
